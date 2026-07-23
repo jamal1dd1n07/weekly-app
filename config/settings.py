f4867ac580b4c2802ebd1e9832890b9feec0a380
JamoliddinXolmatov <xolmatovjamoliddin588@gmail.com>
2026-07-23 14:51:07 +0500

    Added MyUser

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
 
-    'drf_yasg',
+    # 'drf_yasg',
 
-    'apps.users',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +139,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.MyUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class MyUser(AbstractUser):
+    age = models.IntegerField(null=True, blank=True)
+    location = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Foydalanuvchi"
+        verbose_name_plural = "Foydalanuvchilar"
+        ordering = ['id']
